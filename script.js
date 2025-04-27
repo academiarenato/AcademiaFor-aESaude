0014bdd4c41739c0942b9383336e00f01782ddf4
Nunes025 <131490722+Nunes025@users.noreply.github.com>
2025-04-27 20:45:06 -0300

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', async function (event) {
     const formData = new FormData(form);
 
     try {
-        const response = await fetch('https://formspree.io/f/manonkvy', {
+        const response = await fetch('https://formspree.io/f/xzzrzwpj', {
     method: 'POST',
     body: formData,
     headers: {
